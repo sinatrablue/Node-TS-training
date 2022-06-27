9bb91d1c0a20cf953eedc327a7ee433e3eabbd39
Robin Simonot <robin.simonot@castel.fr>
2022-06-27 10:18:04 +0000

    dev: First Express server

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const colours = require("colours");
 const express = require("express");
 
 console.log("Starting the server ...".magenta);
+const app = express();
+// configure server with a middleware
+app.use("/url1/url2", (req, res, next) => {
+  console.log("req : ".magenta, req.url);
+});
+
+app.listen(3000, () => {
+  console.log("> Server started OK !".green);
+});
